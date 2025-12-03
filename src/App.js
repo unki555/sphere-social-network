bfa5338742082b1ba29231a744f2533bc41fad6d
unki555 <romasiksamoilenko@gmail.com>
2025-12-03 13:29:51 +0300

    Fix useCallback import for React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 import { createClient } from '@supabase/supabase-js';
 import {
